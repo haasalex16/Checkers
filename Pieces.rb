5095b9f7a53c19c6ba4d739dd1a3b80a76606a43
Alex Haas <alex.carl.haas@gmail.com>
2015-04-02 10:22:50 -0400

    Working move_diffs method

--- Pieces.rb
+++ Pieces.rb
@@ -1,13 +1,21 @@
 class Piece
 
-  def initialize(color, board)
+
+  def initialize(color, pos ,board)
     @king = false
     @color = color
     @board = board
+    @position = pos
 
   end
 
   def perform_side
+
+
+  end
+
+  def attack?
+
 
   end
 
@@ -16,6 +24,30 @@ class Piece
 
 
   end
-  
+
+  def upgrade
+    @king = true
+  end
+
+  def move_diffs
+    @possible_moves = []
+    if @color == :white
+      @possible_moves << [@position.first + 1 , @position.last + 1]
+      @possible_moves << [@position.first + 1 , @position.last - 1]
+    else
+      @possible_moves << [@position.first - 1 , @position.last + 1]
+      @possible_moves << [@position.first - 1 , @position.last - 1]
+    end
+
+    if @king && @color == :white
+      @possible_moves << [@position.first - 1 , @position.last + 1]
+      @possible_moves << [@position.first - 1 , @position.last - 1]
+    elsif @king && @color == :black
+      @possible_moves << [@position.first + 1 , @position.last + 1]
+      @possible_moves << [@position.first + 1 , @position.last - 1]
+    end
+    @possible_moves
+  end
+
 
 end
