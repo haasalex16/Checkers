1a3fb611f7e2a11f7407be09824fba7e45a2d33c
Alex Haas <alex.carl.haas@gmail.com>
2015-04-02 10:04:40 -0400

    Set up game skeliton

---
+++ Pieces.rb
@@ -0,0 +1,21 @@
+class Piece
+
+  def initialize(color, board)
+    @king = false
+    @color = color
+    @board = board
+
+  end
+
+  def perform_side
+
+  end
+
+
+  def perform_jump
+
+
+  end
+  
+
+end
